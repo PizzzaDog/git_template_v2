b248f5b2769a421e110d911253db97acb8bbfece
PizzzaDog <7883494@gmail.com>
2023-01-12 18:09:21 +0100

    conflict resolved commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
-        System.out.println("LOG INFO");
+        System.out.println("New Log fresh smell good");
         System.out.println("ADDITIONAL LOG INFO");
     }
 }
