e720f605802e1fb85645e021d3e724837e9177b1
PizzzaDog <7883494@gmail.com>
2023-01-12 18:10:23 +0100

    additional log

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("LOG INFO");
+        System.out.println("ADDITIONAL LOG INFO");
     }
 }
